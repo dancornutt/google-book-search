2a350d333f4671bc68809875c627e378360d5030
Dan Cornutt <dancornutt@Dans-MacBook-Pro.local>
2021-02-26 08:18:26 -0800

    built app

---
+++ client/build/precache-manifest.ca808057fe887cc692583f4e21e544af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f6d595e30ec048ba76d25a9487647a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98995aaabf70f8b846a6",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "1844b1604be30ddcc6b3",
+    "url": "/static/css/main.baacc1f7.chunk.css"
+  },
+  {
+    "revision": "98995aaabf70f8b846a6",
+    "url": "/static/js/2.cb29be17.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.cb29be17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1844b1604be30ddcc6b3",
+    "url": "/static/js/main.13ef51fd.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
